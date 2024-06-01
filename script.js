3859d6103cb0c055612741bf11987a746a0f7be8
NehaThombre03 <nehathombre294@gmail.com>
2024-06-01 10:11:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+
+for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400 ? header.classList.add("active")
+    : header.classList.remove("active");
+}); 
